93c34eda99c8a4624d317efeebb9082fa062df52
Tekkieware <tekkieware@gmail.com>
2023-09-09 22:58:31 +0100

    update

--- task/application/urls.py
+++ task/application/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-   path('details', views.get_intern_details, name='intern_details')
+   path('', views.get_intern_details, name='intern_details')
+   
 ]
--- task/application/views.py
+++ task/application/views.py
@@ -5,8 +5,7 @@ from django.http import JsonResponse
 from datetime import date, datetime, timezone
 
 
-# Create your views here.
-
+#geting the current day from the integer values returned from date.today()
 def get_current_day():
     today = date.today()
 
@@ -29,16 +28,19 @@ def get_current_day():
 
 @api_view(['GET'])
 def get_intern_details(request):
-    current_day = get_current_day()
-    utc_time = datetime.now(timezone.utc)
+    if request.method == 'GET':
+        slack_name = request.GET.get('slack_name')
+        track = request.GET.get('track')
+        current_day = get_current_day()
+        utc_time = datetime.now(timezone.utc)
     
-    details = {
-                "slack_name": "example_name",
-                "current_day": current_day,    
-                "utc_time": utc_time,
-                "track": utc_time,
-                "github_file_url": "https://github.com/username/repo/blob/main/file_name.ext",
-                "github_repo_url": "https://github.com/username/repo",
-                "status_code": 200
-               }
+        details = {
+                    "slack_name": slack_name,
+                    "current_day": current_day,    
+                    "utc_time": utc_time,
+                    "track": track,
+                    "github_file_url": "https://github.com/Tekkieware/HNGx-backend-task1/blob/main/task/application/views.py",
+                    "github_repo_url": "https://github.com/Tekkieware/HNGx-backend-task1",
+                    "status_code": 200
+                }
     return JsonResponse(details, status=status.HTTP_200_OK)
