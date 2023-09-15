8a66a9567af8a30f32d636925db9bed95b465b29
Tekkieware <tekkieware@gmail.com>
2023-09-15 16:44:50 +0100

    url fix

--- task/application/urls.py
+++ task/application/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
    
    path('', views.create_user, name='create_user'),
-   path('<user_id>', views.tweak_user, name='tweak_user'),
-   path('intern_details', views.get_intern_details, name='intern_details'),
+   path('/<user_id>', views.tweak_user, name='tweak_user'),
+   path('/intern_details', views.get_intern_details, name='intern_details'),
    
 ]
